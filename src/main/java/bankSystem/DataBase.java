fd6bf66ae539925bc97f3ed1cc29f23589716d90
Anton <112724184+kennytypo@users.noreply.github.com>
2023-05-26 12:05:10 +0300

    Update DataBase.java

--- src/main/java/bankSystem/DataBase.java
+++ src/main/java/bankSystem/DataBase.java
@@ -3,7 +3,7 @@ import java.sql.*;
 public class DataBase {
     private static final String CONNECTION = "jdbc:postgresql://localhost:5432/banksAccounts";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "83Ahatoz";
+    private static final String PASSWORD = "password";
 
     public static Connection connection() throws SQLException {
             return DriverManager.getConnection(CONNECTION,USER,PASSWORD);
